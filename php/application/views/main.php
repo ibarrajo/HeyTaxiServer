5cc56dee3f27234fe85bd5e795cf0af1c8311d49
Josue Ibarra <josue@elninja.com>
2013-10-20 11:03:58 -0700

    forgot a param

--- php/application/views/main.php
+++ php/application/views/main.php
@@ -11,7 +11,7 @@ $this->load->view('inc/header');
   }
 </style>
 <body>
-	<div data-ng-app>
+	<div data-ng-app="gotaxi">
 		<div data-ng-controller="GoTaxiCtrl">
 			<google-map center="center" id="map-canvas"  draggable="true" zoom="zoom" markers="markers" mark-click="true" style="height: 400px"></google-map>
 			<button data-ng-click="getCoordinates()">Get Coordinates</button>
