fd0a5510cc53d55cc4e125699311f75efa6d20cf
Josue Ibarra <josue@elninja.com>
2013-10-20 10:13:58 -0700

    setting up the map canvas

--- php/application/views/main.php
+++ php/application/views/main.php
@@ -1,10 +1,20 @@
 <?php
 $this->load->view('inc/header');
 ?>
+<style>
+  #map-canvas {
+    height: 100%;
+    width: 100%;
+    margin: 0px;
+    padding: 0px
+  }
+</style>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+
 <body>
 	<div data-ng-app>
 		<div data-ng-controller="GoTaxiCtrl">
-			<google-map center="center" draggable="true" zoom="zoom" markers="markers" mark-click="true" style="height: 400px"></google-map>
+			<google-map center="center" id="map-canvas"  draggable="true" zoom="zoom" markers="markers" mark-click="true" style="height: 400px"></google-map>
 			<button data-ng-click="getCoordinates()">Get Coordinates</button>
 		</div>
 	</div>
