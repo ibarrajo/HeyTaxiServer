5aadd5b2eea020dee73c010c363e85b7208a72ef
Josue Ibarra <josue@elninja.com>
2013-10-19 23:30:17 -0700

    ?

--- php/application/views/main.php
+++ php/application/views/main.php
@@ -1,29 +1,37 @@
 <?php
 $this->load->view('inc/header');
 ?>
+<style>
+  html, body, #map-canvas {
+    height: 100%;
+    margin: 0px;
+    padding: 0px
+  }
+</style>
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
 <script>
- // This example adds a marker to indicate the position
+// This example adds a marker to indicate the position
 // of Bondi Beach in Sydney, Australia
 function initialize() {
-  var mapOptions = {
-    zoom: 4,
-    center: new google.maps.LatLng(latitude, longitude),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-                                mapOptions);
+var mapOptions = {
+zoom: 4,
+center: new google.maps.LatLng(-33, 151),
+mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+var map = new google.maps.Map(document.getElementById('map-canvas'),
+                            mapOptions);
 
-  var image = 'img/beachflag.png';
-  var myLatLng = new google.maps.LatLng(latitude, longitude);
-  var beachMarker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      icon: image
-  });
+var image = 'img/beachflag.png';
+var myLatLng = new google.maps.LatLng(-33.890542, 151.274856);
+var beachMarker = new google.maps.Marker({
+  position: myLatLng,
+  map: map,
+  icon: image
+});
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
 </script>
 <body>
 	<div data-ng-app>
